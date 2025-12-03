84f1f13a128cef209ce1b676b802c230cf0c0839
Peehu Mishra <peehumishra1308@gmail.com>
2025-12-04 00:03:45 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-amber-400 text-white'>
+        <p>this is the trial for css</p>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
